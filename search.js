48b1dcca390758e76a01be7175c994074d104210
Steve <stephenkaruru05@gmail.com>
2023-06-09 18:50:05 +0300

    another repo

---
+++ search.js
@@ -0,0 +1,23 @@
+/* let openPage = document.getElementById("search")
+        function openPage() {
+            var x = document.getElementById("search").value;
+
+            if (x === "dog") {
+                window.open("/index.html");
+            }
+
+            if (x === "cat") {
+                window.open("/customizedalert.html");
+            }
+
+        }
+ */
+
+        document.querySelector("#search-form").onsubmit = function(e) {
+            e.preventDefault();
+              // Grab whatever the user typed in
+              console.log("It worked");
+              let searchTermInput = document.querySelector("#search-id").value.trim();
+              let end = "https://api.themoviedb.org/3/search/movie?api_key=473716561aeaecdcec4a3a598dae6440&language=en-US&query=" + searchTermInput + "&page=1&include_adult=false";
+              ajax(end);
+            }       
